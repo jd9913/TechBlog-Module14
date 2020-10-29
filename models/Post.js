e8c3d8db5ea5fc725104d37e1798ab089a2d6c37
Jennifer Schlierman <jd9913@gmail.com>
2020-10-29 09:18:23 -0700

    updated with given files

--- models/Post.js
+++ models/Post.js
@@ -1,44 +1,16 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-// create our Post model
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
 
+class Post extends Model {}
 
-
-// create fields/columns for Post model
 Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      post_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isURL: true
-        }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post'
-    }
-  );
-  
-  module.exports = Post;
-  
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Post;
