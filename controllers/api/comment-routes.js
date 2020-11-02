985b542e84a886a8bc23264e7ed3be2af1e57476
Jennifer Schlierman <jd9913@gmail.com>
2020-11-02 07:24:19 -0700

    small changes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,7 +12,6 @@ router.get('/', (req, res) => {
     });
 });
 
-
 router.post("/", withAuth, (req, res) => {
   Comment.create({ ...req.body, userId: req.session.userId })
     .then(newComment => {
